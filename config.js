033b21af0a369f68e2547169e597526558d555e9
Minhhunt <85287021+Minhhunt@users.noreply.github.com>
2021-06-03 17:17:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introTitle: 'Chào Phươngg!',
+    introDesc: 'Mặc dù
+    đã nhiều lần cố gắng
+    bắt chuyện với em 
+    nhưng anh đều thất bại`,
+    btnIntro: 'vậy sao',
+    title: 'Mình có thể làm quen chứ 😙',
+    desc: 'Nhá nhá nháa ',
+    btnYes: 'Được thui',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em bên cạnh em <3',
+    btnReply: 'Em biết không',
+    reply: 'Em sẽ ib với anhhh',
+    mess: 'zeeeeeeeee',
+    messDesc: 'Thế thì bắt đầu thuiiiiii',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/Minhunt131/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
