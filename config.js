f57f9dbcb075dea741e3068671132fd772ac24d6
Minhhunt <85287021+Minhhunt@users.noreply.github.com>
2021-06-03 20:28:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Chào Phươngg!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Mình có thể nói chuyện với nhau chứ 😙',
+    desc: 'Nhá nhá nháaaaaaaa ',
+    btnYes: 'Được thuiiiii',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    question:'Vậy chúng ta bắt đầu cuộc trò chuyện thôii',
+    btnReply: 'Câu trả lời của em',
+    reply: 'Bắt đầu thôiiii',
+    mess: 'Anh biết mà 🥰. Hihiii',
+    messDesc: 'Ib cho anh nhé <3',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com'
     }
